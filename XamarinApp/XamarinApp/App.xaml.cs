dd06b8917e63dbe0b5a3851ad1d34545d72edc95
Amac <afmckinney@gmail.com>
2017-08-11 23:07:14 -0700

    Added Platform Specific Images and Application Images
    Application Images can be changed in the info.plist file for iOS and the
    properties area for Andriod projects

--- XamarinApp/XamarinApp/App.xaml.cs
+++ XamarinApp/XamarinApp/App.xaml.cs
@@ -13,7 +13,7 @@ namespace XamarinApp
         {
             InitializeComponent();
 
-            MainPage = new EmbeddedImage();
+            MainPage = new PlatformSpecificImages();
         }
 
         protected override void OnStart()
---
+++ XamarinApp/XamarinApp/PlatformSpecificImages.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PlatformSpecificImages : ContentPage
+	{
+		public PlatformSpecificImages ()
+		{
+			InitializeComponent ();
+
+            //btn.Image = (FileImageSource) ImageSource.FromFile(Device.OnPlatform(
+            //        iOS: "clock.png",
+            //        Android: "clock.png"
+            //    ));
+		}
+	}
+}
