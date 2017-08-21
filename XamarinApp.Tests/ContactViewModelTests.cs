e1b39569339eb564447c072d0f8cbdc8b294ef7a
Amac <afmckinney@gmail.com>
2017-08-20 21:55:33 -0700

    Add Unit Tests

---
+++ XamarinApp.Tests/ContactViewModelTests.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+//using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamarinApp.Pages.MVVM.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XamarinApp.Tests
+{
+    //[TestFixture()]
+    [TestClass]
+    public class ContactViewModelTests
+    {
+        private ContactsPageViewModel _viewModel;
+        private Mock<IPageService> _pageService;
+        private Mock<IContactStore> _contactStore;
+
+        //[SetUp]
+        [TestInitialize]
+        public void Setup()
+        {
+            _pageService = new Mock<IPageService>();
+            _contactStore = new Mock<IContactStore>();
+            _viewModel = new ContactsPageViewModel(_contactStore.Object, _pageService.Object);
+        }
+
+        [TestMethod]
+        public void AddContact_WhenCalled_The()
+        {
+            _viewModel.AddContactCommand.Execute(null);
+            Assert.AreEqual(1, _viewModel.Contacts.Count);
+        }
+
+        [TestMethod]
+        public void SelectPlaylist_WhenCalled_TheSelectedItemShouldBeDeselected()
+        {
+            var contact = new ContactViewModel();
+            _viewModel.Contacts.Add(contact);
+
+            _viewModel.SelectContactCommand.Execute(contact);
+
+            Assert.IsNull(_viewModel.SelectedContact);
+        }
+
+    }
+}
---
+++ XamarinApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XamarinApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XamarinApp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d08e631-4b53-4567-8a09-9b927a839969")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
